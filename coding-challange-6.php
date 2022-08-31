2b0c1f31afe1b7f8a528a199f9e5f6fdacff2070
David Broad <davidbroad@servicecertainty.co.uk>
2022-08-31 17:17:41 +0100

    palindrome challenge

--- coding-challange-6.php
+++ coding-challange-6.php
@@ -2,7 +2,7 @@
 // Given a list of words find the indices of words that when joined will form a palindrome.  
 // e.g. given the works ["code", "edoc", "da", "d"] return [(0,1), (1,0), (2,3)]
 
-function reverseString($string){
+function reverseString1($string){
 	$length = strlen($string);
 	$stringReversed = '';
 	for($i = 0; $i < $length; $i++){
@@ -11,14 +11,14 @@ function reverseString($string){
 	return $stringReversed;
 }
 
-function findPalindromes ($words) {
+function findPalindromes1 ($words) {
 	$array = [];
 	$length = count($words);
 	for ($i = 0; $i < $length; $i++){
 		for ($j = 0; $j < $length; $j++){
 			if ($i != $j){
 				$joinedString = $words[$i] . $words[$j];
-				if ($joinedString == reverseString($joinedString)){
+				if ($joinedString == reverseString1($joinedString)){
 					array_push($array, "({$i}, {$j})");
 				}
 			}
@@ -27,4 +27,47 @@ function findPalindromes ($words) {
 	return $array;
 }
 
-print_r(findPalindromes(["code", "edoc", "da", "d"]));
+print_r(findPalindromes1(["code", "edoc", "da", "d"]));
+
+
+
+function reverseString2($string){
+	$length = strlen($string);
+	$stringReversed = '';
+	for($i = 0; $i < $length; $i++){
+		$stringReversed = $string[$i] . $stringReversed;
+	}	
+	return $stringReversed;
+}
+
+function findPalindromes2 ($words) {
+	$length = count($words);
+	$array = [];
+	$wordsReversed = [];
+	$wordsReversedTrimLeft = [];
+	$wordsReversedTrimRight = [];
+	for ($i = 0; $i < $length; $i++) {
+		$reversedWord = reverseString2($words[$i]);
+		$wordsReversed[$i] = $reversedWord;
+		$wordsReversedTrimLeft[$i] = substr($reversedWord, 1);
+		$wordsReversedTrimRight[$i] = substr($reversedWord, 0, -1);
+	}
+	for ($i = 0; $i < $length; $i++){
+		$index = array_search($words[$i], $wordsReversed);
+		if($index == $i){
+			$index = false;
+		}
+		if(!is_int($index) && $index != $i){
+			$index = array_search($words[$i], $wordsReversedTrimLeft);
+		}
+		if(!is_int($index) && $index != $i){
+			$index = array_search($words[$i], $wordsReversedTrimRight);
+		}
+		if(is_int($index)){
+			array_push($array, "({$i}, {$index})");
+		}
+	}
+	return $array;
+}
+
+print_r(findPalindromes2(["code", "edoc", "da", "d", "cat", "ac", "oll", "ello"]));
